e59b12b93fd280e99ed445ff4f1bb108e0e8dad5
henri <henri.rapeli@gmail.com>
2023-02-15 21:25:08 +0200

    Added a new exception

--- matrices/exceptions.py
+++ matrices/exceptions.py
@@ -7,3 +7,9 @@ class DimensionError(Exception):
     Exception for a situation where the matrix dimensions
     are incorrect
     """
+
+
+class MatrixError(Exception):
+    """
+    Exception for a situation where the matrix is not a nested list
+    """
