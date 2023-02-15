7856604835a2f08f3714d5d5ecfe92edcb1e30d9
henri <henri.rapeli@gmail.com>
2023-02-15 21:43:58 +0200

    Removed an unused custom exception

--- matrices/exceptions.py
+++ matrices/exceptions.py
@@ -7,9 +7,3 @@ class DimensionError(Exception):
     Exception for a situation where the matrix dimensions
     are incorrect
     """
-
-
-class MatrixError(Exception):
-    """
-    Exception for a situation where the matrix is not a nested list
-    """
